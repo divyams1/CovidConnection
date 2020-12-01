bba65d2aa7e23950c3f0bcfc4e648303fd090f69
App Academy Student <email@email.com>
2020-11-30 23:42:53 -0500

    user auth

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const favors = require("./routes/api/favors");
+const  bodyParser = require('body-parser');
+const passport = require('passport');
+
 
 mongoose
     .connect(db, { useNewUrlParser: true })
@@ -9,6 +14,25 @@ mongoose
     .catch( err => ( console.log(err)));
 
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(bodyParser.urlencoded({
+    extended: false 
+
+}))
+
+app.use(passport.initialize());
+
+app.get("/", (req, res) => {
+const newUser = new User({
+        username: "jim",
+        email: "jim@jim.jim",
+        password: "fyi1234"
+      });
+
+
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+app.use("/api/users", users)
+app.use("/api/favors", favors)
---
+++ routes/api/favors.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/test", (req, res)  => {
+
+    res.json({msg: "This is the favors route"});
+
+});
+
+
+
+module.exports = router;
+
+
