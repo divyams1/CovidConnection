9a0e67a8e54d140ebab05cbc388a7e3cff6ee2a0
divyams1 <divyamsat@gmail.com>
2020-12-03 17:28:58 -0500

    removed node_modules

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ const User = require('./models/User');
 const bodyParser = require('body-parser');
 const favors = require("./routes/api/favors");
 const passport = require('passport');
+const path = require('path');
 
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => (console.log("Connected to MongoDB")))
