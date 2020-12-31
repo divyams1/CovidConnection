c7d173ce79ad1393bf8414e939ce2b3d217cba25
divyams1 <divyamsat@gmail.com>
2020-12-31 11:58:02 -0500

    removed favors

--- routes/api/favors.js
+++ routes/api/favors.js
@@ -113,7 +113,6 @@ router.post("/",
             favor_title: req.body.favor_title,
             favor_lat: req.body.favor_lat,
             favor_lng: req.body.favor_lng,
-            favor_for_username: req.user.username, 
             favor_status: false
             // req.body.favor_status,
         });
