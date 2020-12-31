b32ae862c07e766c4eea94405ed0a209ca3e8b40
divyams1 <divyamsat@gmail.com>
2020-12-31 12:00:10 -0500

    added profiles

--- frontend/src/components/profiles/profile.jsx
+++ frontend/src/components/profiles/profile.jsx
@@ -11,7 +11,6 @@ class Profile extends React.Component {
         this.handleFavors = this.handleFavors.bind(this);
         this.handleNoFavors = this.handleNoFavors.bind(this);
         this.renderForm = this.renderForm.bind(this);
-        this.updateFavor = this.props.updateFavor.bind(this);
     }
     
     componentDidMount() {
@@ -55,11 +54,11 @@ class Profile extends React.Component {
               </>
     )}
 
-  handleFavors() {
+    handleFavors(){
 
-    if (this.props.favors) {
-
-
+    if(this.props.favors){
+      
+     
       return (
       <div className="prof-favors">
          {this.props.favors
@@ -70,7 +69,7 @@ class Profile extends React.Component {
       )}
 
 
-  }
+      }
     
     render() {
 
@@ -82,7 +81,7 @@ class Profile extends React.Component {
             <ProfileNavContainer />
             <div className="prof-favors">
               
-                <h1>  Welcome {this.props.user_profile_id}!  </h1>
+              <h1>  Welcome {this.props.currentUser.username}!  </h1>
               <h2 className="prof-nofavors"> Your quest to make this world a better place.... These are the favors you have done! </h2>
                <h3>  Currently experiencing Covid symptoms?  Visit our info page for tips handling stress.      <NavLink to="/covid">Covid Help</NavLink>        </h3>   
 
@@ -91,7 +90,7 @@ class Profile extends React.Component {
               <div>
                 {this.handleFavors()}
 
-                {/* {this.handleNoFavors()} */}
+                {this.handleNoFavors()}
               </div>
               </div>
               <div  className="banners">
@@ -119,3 +118,4 @@ class Profile extends React.Component {
 }
 
 export default Profile;
+
