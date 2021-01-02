84ffaf77af1093cefb44189827db6ff37c1b00ba
divyams1 <divyamsat@gmail.com>
2021-01-02 09:38:20 -0500

    added newsfeed filters

--- frontend/src/components/newsfeed/newsfeed.jsx
+++ frontend/src/components/newsfeed/newsfeed.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 class NewsFeed extends React.Component {
     componentDidMount() {
         this.props.fetchFavors();
+        this.state = { myFavors: false, favorRequests: false, userSearch: false, forUser: ''}
     }
     render() {
         let favors = ( <div> </div>)
@@ -14,10 +15,13 @@ class NewsFeed extends React.Component {
             <div>
             <h2> {favor.favor_title} </h2>   
             <p> {favor.favor_description} </p>
+            <p> {favor.favor_for_username}</p>
             </div>)
         })
-       
         }
+         favors = ( this.state.myFavors? favors.filter( favor => this.props.currentUser.id === favor.favor_for_user_id) : favors)
+         favors = ( this.state.favorRequests? favors.filter( favor => favor.status === true) : favors )
+        
         return(
             <div>
                 <h1> View Favors Other Users Have Made </h1>
