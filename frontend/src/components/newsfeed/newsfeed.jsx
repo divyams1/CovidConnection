3e41e8f5c23af59eab8657b8689c62c42d5d5b00
divyams1 <divyamsat@gmail.com>
2021-01-04 09:47:59 -0500

    added profile_show page

--- frontend/src/components/newsfeed/newsfeed.jsx
+++ frontend/src/components/newsfeed/newsfeed.jsx
@@ -1,33 +1,61 @@
-import React from 'react'
+import React from 'react';
+import {NavLink, Link} from 'react-router-dom';
 
 
 class NewsFeed extends React.Component {
     constructor(props) {
         super(props)
         this.state = { myFavors: false, favorRequests: false, userSearch: false, forUser: ''}
+        this.userShow = this.userShow.bind(this);
+        this.requestShow = this.requestShow.bind(this);
     }
     componentDidMount() {
         this.props.fetchFavors();
     }
+
+    requestShow() {
+        if (!this.state.favorRequests) {
+            this.setState( { favorRequests: true})
+        } else {
+            this.setState( { favorRequests: false})
+        }
+    }
+      userShow() {
+        if ( !this.state.myFavors ) {
+            this.setState( { myFavors: true})
+        } else {
+            this.setState( {myFavors: false })
+        }
+    }
+
+    updateName() {
+        return e=> {
+            this.setState( { forUser: e.currentTarget.value })
+        }
+
+    }
     render() {
         
-        let favors = Object.values(this.props.favors.data) || [];
-        debugger
+        let favors = (this.props.favors.data) || [];
+        
         favors = ( this.state.myFavors? favors.filter( favor => this.props.currentUser.id === favor.favor_for_user_id) : favors)
+        
         favors = ( this.state.favorRequests? favors.filter( favor => favor.status === true) : favors )
         favors = ( this.state.userSearch? favors.filter( favor => favor.favor_for_username === this.state.forUser) : favors)
-        favors = this.props.favors.data.map( favor => {
-
+        favors = favors.map( favor => {
              return(   
             <div>
-            <h2> {favor.favor_title} </h2>   
+            <h1> {favor.favor_title} </h1>
             <p> {favor.favor_description} </p>
-            <p> {favor.favor_for_username}</p>
+            <Link to={`/user/${favor.favor_for_user_id}`} >{favor.favor_for_username}  </Link>
             </div>)
         })
-         
+    
         return(
             <div>
+                <button onClick={this.userShow}>View Your Favors</button>
+                <button onClick={this.requestShow}> View Requests Others Have Made</button>
+                {/* <input type="text"  onChange={this.updateName}></input> */}
                 <h1> View Favors Other Users Have Made </h1>
                 {favors}
             </div>
