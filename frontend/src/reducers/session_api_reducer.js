45e84cb8f8d2206e0192039b57bd44e55e1a211c
Anthony Collichio <collich55@gmail.com>
2021-01-19 19:09:18 -0500

    console log in reducer

--- frontend/src/reducers/session_api_reducer.js
+++ frontend/src/reducers/session_api_reducer.js
@@ -11,7 +11,7 @@ const initialState = {
 export default function(state = initialState, action) {
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      
+      console.log(action)
       return {
         ...state,
         isAuthenticated: !!action.currentUser,
